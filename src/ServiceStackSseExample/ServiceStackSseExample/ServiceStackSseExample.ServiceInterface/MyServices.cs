71074e87dfcc3dcece773b718c86ea69ff07f8d6
Darren Reid <darren.j.reid@gmail.com>
2016-05-08 19:50:31 +1000

    Add SS server

---
+++ src/ServiceStackSseExample/ServiceStackSseExample/ServiceStackSseExample.ServiceInterface/MyServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using ServiceStackSseExample.ServiceModel;
+
+namespace ServiceStackSseExample.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public IServerEvents ServerEvents { get; set; }
+
+        public GotoSlidePageResponse Any(GotoSlidePage request)
+        {
+            request.PageNumber++;
+            ServerEvents.NotifyAll(request);
+            return request.ConvertTo<GotoSlidePageResponse>();
+        }
+    }
+}
---
+++ src/ServiceStackSseExample/ServiceStackSseExample/ServiceStackSseExample.ServiceModel/Hello.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+
+namespace ServiceStackSseExample.ServiceModel
+{
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    [Route("/slide")]
+    public class GotoSlidePage
+    {
+        public int PageNumber { get; set; }
+    }
+
+    public class GotoSlidePageResponse
+    {
+        public int PageNumber { get; set; }
+    }
+}
